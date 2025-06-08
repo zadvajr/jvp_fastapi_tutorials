4e4d400df0d34dfd0279e42958fe9681243ac09a
unknown <danielzadva@gmail.com>
2025-06-08 02:11:24 +0100

    path parameters with enum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -25,3 +26,17 @@ async def get_current_user():
 @app.get("/users/{user_id}")
 async def get_user(user_id: str):
     return {"user_id": user_id}
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+    if food_name.value == "fruits":
+        return {"food_name": food_name, "message": "you are still healthy, but like sweet things"}
+    return {"food_name": food_name, "message": "I like chocolate milk"}
+    
